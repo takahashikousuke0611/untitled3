0f9277b22221067c5b3c0e30d14d490d544c4215
unknown <241006>
2025-05-09 11:51:16 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか
+// ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("髙橋　煌祐");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Hello world");
+        }
+    }
+}
